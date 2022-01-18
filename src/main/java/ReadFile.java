89c2f79558dc3a72ea7ce8d3a46a70a304cd93d1
LiorCohen1 <Liorcohen122003@gmail.com>
2022-01-18 12:50:58 +0200

    A new interface that is responsible for reading from a file

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public interface ReadFile {
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public interface ReadFile<T,T2> {
+    public  List<ArrayList<T>> FileIntoList(String path) throws IOException;
+    public T newItem(Iterator <T2> csvRecordIterator);
+
+
 }
